51294f077575e9495356f8fae2a4eec406a3c897
zhixiang.yuan <im.yuanzx@gmail.com>
2018-08-17 23:12:37 +0800

    创建 netty server 代码

---
+++ netty_demo/src/main/java/nettyserver/NettyConfig.java
@@ -0,0 +1,71 @@
+package nettyserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+
+/**
+ * <p> netty 配置类 </p>
+ *
+ * @author zhixiang.yuan
+ * @create 2018/08/17 18:29
+ */
+public class NettyConfig {
+
+    private int port;
+
+    public NettyConfig(int port){
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new NettyConfig(port).start();
+    }
+
+    public void start(){
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap().group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("decoder", new StringDecoder());
+                            ch.pipeline().addLast("encoder", new StringEncoder());
+                            ch.pipeline().addLast(new ServerHandler());
+                        }
+
+                    }).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // 绑定端口，开始接收进来的连接
+            ChannelFuture future = bootstrap.bind(port).sync();
+            System.out.println("Server start listen at " + port );
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
---
+++ netty_demo/src/main/java/nettyserver/ServerHandler.java
@@ -0,0 +1,28 @@
+package nettyserver;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * <p> 服务处理器 </p>
+ *
+ * @author zhixiang.yuan
+ * @create 2018/08/17 18:21
+ */
+public class ServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg){
+        System.out.println("server channelRead..");
+        System.out.println(ctx.channel().remoteAddress() + "-> Server :" + msg.toString());
+        ctx.write("server write" + msg);
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
